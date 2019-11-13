ab6235d6c0d29b996545286d978fdaaff0a09dec
mariusz.kuberczyk <mariusz.kuberczyk@gorrion.pl>
2019-11-13 21:51:33 +0100

    removed awaits from resolver

--- server/src/modules/users/users.resolver.ts
+++ server/src/modules/users/users.resolver.ts
@@ -10,26 +10,26 @@ export class UsersResolver {
 
   @Query(() => [User])
   async findAllUsers(): Promise<User[]> {
-    return await this.usersService.findAll();
+    return this.usersService.findAll();
   }
 
   @Query(() => User)
   async findUser(@Args('id') id: string): Promise<User> {
-    return await this.usersService.findOne(id);
+    return this.usersService.findOne(id);
   }
 
   @Mutation(() => User)
   async addUser(@Args('user') userInput: AddUserInput): Promise<User> {
-    return await this.usersService.add(userInput);
+    return this.usersService.add(userInput);
   }
 
   @Mutation(() => User)
   async deleteUser(@Args('id') id: string): Promise<User> {
-    return await this.usersService.delete(id);
+    return this.usersService.delete(id);
   }
 
   @Mutation(() => User)
   async updateUser(@Args('id') id: string, @Args('user') userInput: UpdateUserInput): Promise<User> {
-    return await this.usersService.update(id, userInput);
+    return this.usersService.update(id, userInput);
   }
 }
