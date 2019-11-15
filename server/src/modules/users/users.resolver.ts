b178ec65ae7d31b6686093fcbc3dbd042cf1b5c7
Marcin Kochanek <marcin.kochanek@gorrion.pl>
2019-11-15 13:42:18 +0100

    added updating/removing user

--- server/src/modules/users/users.resolver.ts
+++ server/src/modules/users/users.resolver.ts
@@ -2,10 +2,11 @@ import { Query, Resolver, Args, Mutation } from '@nestjs/graphql';
 import { User } from './models/user';
 import { UsersService } from './users.service';
 import { AddUserInput } from './models/addUserInput';
+import { UpdateUserInput } from './models/updateUserInput';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User])
   async findAllUsers(): Promise<User[]> {
@@ -15,5 +16,23 @@ export class UsersResolver {
   @Mutation(() => User)
   async addUser(@Args('user') userInput: AddUserInput): Promise<User> {
     return await this.usersService.add(userInput);
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') userId: string,
+    @Args('user') userInput: UpdateUserInput,
+  ): Promise<User> {
+    return await this.usersService.update(userId, userInput);
+  }
+
+  @Mutation(() => Boolean)
+  async removeUser(@Args('id') userId: string): Promise<boolean> {
+    try {
+      await this.usersService.remove(userId);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,15 +1,16 @@
 import { ReturnModelType } from '@typegoose/typegoose';
-import { User } from './models/user';
 import { InjectModel } from '@nestjs/mongoose';
-import { AddUserInput } from './models/addUserInput';
 import { Injectable } from '@nestjs/common';
+import { User } from './models/user';
+import { AddUserInput } from './models/addUserInput';
+import { UpdateUserInput } from './models/updateUserInput';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name)
     private readonly userModel: ReturnModelType<typeof User>,
-  ) { }
+  ) {}
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
@@ -24,4 +25,24 @@ export class UsersService {
     return user.save();
   }
 
+  async update(id: string, dto: UpdateUserInput): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new Error(`Couldn’t find user with id ${id}`);
+    }
+
+    const updatedUser = Object.assign(user, dto);
+    return updatedUser.save();
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new Error(`Couldn’t find user with id ${id}`);
+    }
+
+    return user.remove();
+  }
 }
